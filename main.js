4cc180356befe57ae1ca6b14d7ff29a2cdb7759f
smrpn <samarpan_d@pp.iitr.ac.in>
2021-04-19 14:35:33 +0530

    remove comment
    Signed-off-by: smrpn <samarpan_d@pp.iitr.ac.in>

--- main.js
+++ main.js
@@ -5,37 +5,54 @@ const github = require("@actions/github");
 const context = github.context;
 
 async function main() {
-  const myToken = process.env["GITHUB_TOKEN"];
+  const myToken = core.getInput("token", { required: true });
+  const options = {};
+  let myOutput;
+  let myError;
+  let cwd;
+
+  if ((cwd = core.getInput("cwd"))) {
+    options.cwd = cwd;
+  }
+  
+  benchCmd = ["bench"];
+  if ((cargoBenchName = core.getInput("benchName"))) {
+    benchCmd = benchCmd.concat(["--bench", cargoBenchName]);
+  }
 
   core.debug("### Install Critcmp ###");
   await exec.exec("cargo", ["install", "critcmp"]);
 
   core.debug("### Benchmark starting ###");
-  await exec.exec("cargo", ["bench", "--", "--save-baseline", "changes"]);
+  await exec.exec(
+    "cargo",
+    benchCmd.concat(["--", "--save-baseline", "changes"]),
+    options
+  );
   core.debug("Changes benchmarked");
   await exec.exec("git", ["checkout", "master"]);
   core.debug("Checked out to master branch");
-  await exec.exec("cargo", ["bench", "--", "--save-baseline", "master"]);
+  await exec.exec(
+    "cargo",
+    benchCmd.concat(["--", "--save-baseline", "master"]),
+    options
+  );
   core.debug("Master benchmarked");
 
-  const options = {};
-  let myOutput;
-  let myError;
-
   options.listeners = {
-    stdout: data => {
+    stdout: (data) => {
       myOutput += data.toString();
     },
-    stderr: data => {
+    stderr: (data) => {
       myError += data.toString();
-    }
+    },
   };
 
   await exec.exec("critcmp", ["master", "changes"], options);
   const resultsAsMarkdown = convertToMarkdown(myOutput);
 
   // An authenticated instance of `@octokit/rest`
-  const octokit = new github.GitHub(myToken);
+  const octokit = github.getOctokit(myToken);
 
   const contextObj = { ...context.issue };
 
@@ -44,7 +61,7 @@ async function main() {
       owner: contextObj.owner,
       repo: contextObj.repo,
       issue_number: contextObj.number,
-      body: resultsAsMarkdown
+      body: resultsAsMarkdown,
     });
   } catch (e) {
     // If we can't post to the comment, display results here.
@@ -52,7 +69,7 @@ async function main() {
     // https://github.community/t5/GitHub-Actions/quot-Resource-not-accessible-by-integration-quot-for-adding-a/td-p/33925
     const resultsAsObject = convertToTableObject(myOutput);
     console.table(resultsAsObject);
-
+    console.log("Failed to comment");
     core.debug(e);
     core.debug("Failed to comment");
   }
@@ -60,8 +77,39 @@ async function main() {
   core.debug("Succesfully run!");
 }
 
+function convertDurToSeconds(dur, units) {
+  let seconds;
+  switch (units) {
+    case "s":
+      seconds = dur;
+      break;
+    case "ms":
+      seconds = dur / 1000;
+      break;
+    case "µs":
+      seconds = dur / 1000000;
+      break;
+    case "ns":
+      seconds = dur / 1000000000;
+      break;
+    default:
+      seconds = dur;
+      break;
+  }
+
+  return seconds;
+}
+
+function isSignificant(changesDur, changesErr, masterDur, masterErr) {
+  if (changesDur < masterDur) {
+    return changesDur + changesErr < masterDur || masterDur - masterErr > changesDur;
+  } else {
+    return changesDur - changesErr > masterDur || masterDur + masterErr < changesDur;
+  }
+}
+
 function convertToMarkdown(results) {
-  /* Example results: 
+  /* Example results:
     group                            changes                                master
     -----                            -------                                ------
     character module                 1.03     22.2±0.41ms        ? B/sec    1.00     21.6±0.53ms        ? B/sec
@@ -72,7 +120,7 @@ function convertToMarkdown(results) {
   let resultLines = results.split("\n");
   let benchResults = resultLines
     .slice(2) // skip headers
-    .map(row => row.split(/\s{2,}/)) // split if 2+ spaces together
+    .map((row) => row.split(/\s{2,}/)) // split if 2+ spaces together
     .map(
       ([
         name,
@@ -81,21 +129,52 @@ function convertToMarkdown(results) {
         _changesBandwidth,
         masterFactor,
         masterDuration,
-        _masterBandwidth
+        _masterBandwidth,
       ]) => {
-        changesFactor = Number(changesFactor);
-        masterFactor = Number(masterFactor);
+        let masterUndefined = typeof masterDuration === "undefined";
+        let changesUndefined = typeof changesDuration === "undefined";
 
-        let difference = 100;
-        if (changesFactor < masterFactor) {
-          changesDuration = `**${changesDuration}**`;
-          difference = (2 - masterFactor) * 100;
-        } else if (changesFactor > masterFactor) {
-          masterDuration = `**${masterDuration}**`;
-          difference = changesFactor * 100;
+        if (!name || (masterUndefined && changesUndefined)) {
+          return "";
         }
 
-        return `| ${name} | ${changesDuration} | ${masterDuration} | ${difference}% |`;
+        let difference = "N/A";
+        if (!masterUndefined && !changesUndefined) {
+          changesFactor = Number(changesFactor);
+          masterFactor = Number(masterFactor);
+
+          let changesDurSplit = changesDuration.split('±');
+          let changesUnits = changesDurSplit[1].slice(-2);
+          let changesDurSecs = convertDurToSeconds(changesDurSplit[0], changesUnits);
+          let changesErrorSecs = convertDurToSeconds(changesDurSplit[1].slice(0, -2), changesUnits);
+
+          let masterDurSplit = masterDuration.split('±');
+          let masterUnits = masterDurSplit[1].slice(-2);
+          let masterDurSecs = convertDurToSeconds(masterDurSplit[0], masterUnits);
+          let masterErrorSecs = convertDurToSeconds(masterDurSplit[1].slice(0, -2), masterUnits);
+
+          difference = -(1 - changesDurSecs / masterDurSecs) * 100;
+          difference = (changesDurSecs <= masterDurSecs ? "" : "+") + difference.toFixed(2) + "%";
+          if (isSignificant(changesDurSecs, changesErrorSecs, masterDurSecs, masterErrorSecs)) {
+            if (changesDurSecs < masterDurSecs) {
+              changesDuration = `**${changesDuration}**`;
+            } else if (changesDurSecs > masterDurSecs) {
+              masterDuration = `**${masterDuration}**`;
+            }
+
+            difference = `**${difference}**`;
+          }
+        }
+
+        if (masterUndefined) {
+          masterDuration = "N/A";
+        }
+
+        if (changesUndefined) {
+          changesDuration = "N/A";
+        }
+
+        return `| ${name} | ${changesDuration} | ${masterDuration} | ${difference} |`;
       }
     )
     .join("\n");
@@ -114,7 +193,7 @@ ${benchResults}
 }
 
 function convertToTableObject(results) {
-  /* Example results: 
+  /* Example results:
     group                            changes                                master
     -----                            -------                                ------
     character module                 1.03     22.2±0.41ms        ? B/sec    1.00     21.6±0.53ms        ? B/sec
@@ -125,7 +204,7 @@ function convertToTableObject(results) {
   let resultLines = results.split("\n");
   let benchResults = resultLines
     .slice(2) // skip headers
-    .map(row => row.split(/\s{2,}/)) // split if 2+ spaces together
+    .map((row) => row.split(/\s{2,}/)) // split if 2+ spaces together
     .map(
       ([
         name,
@@ -134,25 +213,26 @@ function convertToTableObject(results) {
         _changesBandwidth,
         masterFactor,
         masterDuration,
-        _masterBandwidth
+        _masterBandwidth,
       ]) => {
         changesFactor = Number(changesFactor);
         masterFactor = Number(masterFactor);
 
-        let difference = 100;
+        let difference = -(1 - changesFactor / masterFactor) * 100;
+        difference =
+          (changesFactor <= masterFactor ? "" : "+") +
+          difference.toPrecision(2);
         if (changesFactor < masterFactor) {
           changesDuration = `**${changesDuration}**`;
-          difference = (2 - masterFactor) * 100;
         } else if (changesFactor > masterFactor) {
           masterDuration = `**${masterDuration}**`;
-          difference = changesFactor * 100;
         }
 
         return {
           name,
           changesDuration,
           masterDuration,
-          difference
+          difference,
         };
       }
     );
@@ -169,3 +249,4 @@ function convertToTableObject(results) {
     core.setFailed(`Unhanded error:\n${e}`);
   }
 })();
+
